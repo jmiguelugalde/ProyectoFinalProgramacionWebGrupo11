6950cbcfd4f25f893d682deacbe46f1f6e3b5e58
Jose Ugalde <tu-correo@ejemplo.com>
2025-08-16 00:30:41 -0600

    feat: versión funcional backend+frontend (UI clara estilo Win11, auth, dashboard)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./routes/AuthContext";
 import App from "./App";
-import "./styles.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
